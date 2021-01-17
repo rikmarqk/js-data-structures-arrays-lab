d5edcf168654e23632f2def4e44e41cbd20f752c
rikmarqk <rikmarqk@gmail.com>
2021-01-17 16:32:28 -0600

    commit tree 4f759833e090db5f2715e1c1767311ccf23165e4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+let drivers = ["Milo","Otis","Garfield"];
+let arrayFunctions = (drivers) => {
+    drivers.push("Ralph")
+    drivers.pop()
+    drivers.unshift("Bob")
+    drivers.shift()
+    drivers.pop()
+    drivers.push('Garfield')
+    drivers.shift()
+    drivers.unshift('Milo')
+    const drivers1 = [...drivers, 'Broom']
+    const drivers2 = ['Broom', ...drivers]
+    const copyOfDrivers = drivers.slice()
+    drivers.pop
+    drivers.unshift
+}
